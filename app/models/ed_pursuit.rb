d61692749e668401092a5b95f57acdbfe5d0fd1b
Soundwood <j.benhogan@gmail.com>
2020-09-09 17:40:03 -0500

    added validations

--- app/models/ed_pursuit.rb
+++ app/models/ed_pursuit.rb
@@ -1,3 +1,7 @@
 class EdPursuit < ApplicationRecord
     belongs_to :user
+    has_many :notes
+    validates :name, presence: true
+    validates :subject, presence: true
+    validates :provider, presence: true
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ApplicationRecord
   belongs_to :user
   belongs_to :ed_pursuit
+  validates :title, presence: true
 end

03e7fee8d865e4474c717f01d0becc7fd04354d9
Soundwood <j.benhogan@gmail.com>
2020-09-09 19:41:13 -0500

    added helper methods and login checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+    
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+    
+    def require_logged_in
+        redirect_to controller: 'sessions', action: 'new' unless current_user
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to login_path if !logged_in?
+    end
 end

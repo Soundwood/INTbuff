c375a7d28a213a9558e5496b8966058543c7e038
Soundwood <j.benhogan@gmail.com>
2020-09-09 19:46:12 -0500

    added private methods and before_actions

--- app/controllers/ed_pursuits_controller.rb
+++ app/controllers/ed_pursuits_controller.rb
@@ -1,2 +1,48 @@
 class EdPursuitsController < ApplicationController
+    before_action :set_ed_pursuit, only:[:show, :edit, :update]
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @ed_pursuit = EdPursuit.new
+    end
+
+    def create
+        @ed_pursuit = EdPursuit.create(ed_pursuit_params)
+        @ed_pursuit.user_id = session[:user_id]
+        if @ed_pursuit.save
+            redirect_to ed_pursuit_path(@ed_pursuit)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @ed_pursuits = EdPursuit.all
+    end
+
+    def show
+        @ed_pursuit = EdPursuit.find_by_id(params[:id])
+    end
+
+    def edit 
+    end
+
+    def update
+        if @ed_pursuit.update(ed_pursuit_params)
+          redirect_to ed_pursuit_path(@ed_pursuit)
+        else
+          render :edit
+        end
+    end
+
+    private
+
+    def ed_pursuit_params
+        params.require(:ed_pursuit).permit(:ed_type_id, :name, :subject, :provider, :instructor, :start_date, :duration_d)
+    end
+
+    def set_ed_pursuit
+        @ed_pursuit = EdPursuit.find_by(params[:id])
+        redirect_to ed_pursuits_path if !@ed_pursuit
+    end
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,60 @@
 class NotesController < ApplicationController
+    before_action :set_note, only:[:show, :edit, :update]
+    before_action :redirect_if_not_logged_in
+
+    def index
+    end
+
+    def new
+        if @ed_pursuit = EdPursuit.find_by_id(params[:ed_pursuit_id])
+            @note = @ed_pursuit.notes.build
+        else
+            @note = Note.new
+        end
+    end
+    
+    def create
+        @note = current_user.notes.build(note_params)
+        @note.user_id = current_user.id
+        if @note.save
+            redirect_to note_path(@note)
+        else
+            render :new
+        end
+    end
+    
+    def show
+        @note = Note.find_by_id(params[:id])
+    end
+
+    def index
+        if params[:ed_pursuit_id]
+            @ed_pursuit = EdPursuit.find_by_id(params[:ed_pursuit_id])
+            @index_title = @ed_pursuit.name
+            @notes = @ed_pursuit.notes.where(user_id: current_user)
+        else
+            @index_title = "All Educational Pursuits"
+            @notes = Note.all.where(user_id: current_user)
+        end
+    end
+
+    def update
+        if @note.update(note_params)
+          redirect_to note_path(@note)
+        else
+          render :edit
+        end
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:ed_pursuit_id, :title, :content)
+    end
+
+    def set_note
+        @note = Note.find_by(params[:id])
+        redirect_to notes_path if !@note
+    end
+
 end

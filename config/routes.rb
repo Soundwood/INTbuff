663ca4cce9f9adfb1522173fe3c10bcf0a713755
Soundwood <j.benhogan@gmail.com>
2020-09-10 19:29:04 -0500

    changed root userhome and welcomepage to fallback

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   # custom routes
-  get '/' => 'sessions#welcome'
+  get '/' => 'users#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
   delete '/logout' => 'sessions#destroy'
+  get '/welcome' => 'sessions#welcome'
+
+  root to: "user#show"
 
   # resource defined routes
   resources :notes
   resources :ed_pursuits do
-    resources :notes, only: [:new, :index, :show]
+    resources :notes, only: [:new, :index, :create]
   end
   resources :users, only: [:new, :create, :destroy, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

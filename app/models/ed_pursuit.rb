4506b5b351022b244b0f6a1bf27624ca5865cdcf
Soundwood <j.benhogan@gmail.com>
2020-09-14 09:26:08 -0500

    added instance methods provider_attr and provider_name

--- app/models/ed_pursuit.rb
+++ app/models/ed_pursuit.rb
@@ -2,7 +2,16 @@ class EdPursuit < ApplicationRecord
     belongs_to :user
     has_many :notes
     belongs_to :ed_type
+    belongs_to :provider
     validates :name, presence: true
     validates :subject, presence: true
     validates :provider, presence: true
+
+    def provider_attributes=(attributes)
+        self.provider = Provider.find_or_create_by(attributes) if !attributes['name'].empty?
+        self.provider
+    end
+    def provider_name
+        provider.try(:name)
+    end
 end

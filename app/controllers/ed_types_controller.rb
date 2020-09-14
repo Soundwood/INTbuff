743ee22d63f616296c4976c263d1041ebd4ceca1
Soundwood <j.benhogan@gmail.com>
2020-09-13 19:18:05 -0500

    added set_ed_type method

--- app/controllers/ed_types_controller.rb
+++ app/controllers/ed_types_controller.rb
@@ -1,2 +1,10 @@
 class EdTypesController < ApplicationController
+    
+    
+    private
+
+    def set_ed_type
+        @ed_type = EdType.find_by_id(params[:id])
+        redirect_to ed_types_path if !@ed_type
+    end
 end

225ff820843a716e1d844e563d29258709e8b97a
Soundwood <j.benhogan@gmail.com>
2020-09-14 09:28:15 -0500

    added validations and scope method

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,7 @@
 class Provider < ApplicationRecord
     has_many :users, through: :ed_pursuits
     has_many :ed_pursuits
+    validates :name, presence: true
+
+    scope :order_by_name, -> {order(:name)}
 end

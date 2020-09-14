c177784ea3d0f721437b75b19d6f19bd9edd7e3b
Soundwood <j.benhogan@gmail.com>
2020-09-14 09:27:40 -0500

    added validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,9 @@ class Note < ApplicationRecord
   belongs_to :user
   belongs_to :ed_pursuit
   validates :title, presence: true
+  validates :content, presence: true
+  validates :ed_pursuit_id, presence: true
+
+  # scope :for_user, -> (userid) { where("user_id" == ?, userid)}
+  
 end
